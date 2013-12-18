d836ec0cf44ce49960bf63a9383607ee5ecaae8e
Eric Shamow <eric.shamow@gmail.com>
2013-12-18 12:04:12 -0800

    Added get_servers method

--- lib/contrail/ec2.rb
+++ lib/contrail/ec2.rb
@@ -11,5 +11,37 @@ module Contrail
         :region   => configdata['aws_default_region']
       )
     end
+    def get_keys(options = {:name => nil})
+      @connection.key_pairs.select do |key_pair|
+        next unless (options[:name] == nil) || (key_pair.name == options[:name])
+        key_pair
+      end
+    end
+    def get_images(options = {:id => nil})
+      @connection.images.select do |image|
+        next unless (options[:id] == nil) || (image.id == options[:id])
+        image
+      end
+    end
+    def get_servers(options = {
+      :id => nil,
+      :private_ip_address => nil,
+      :public_ip_address => nil,
+      :dns_name => nil,
+      :image_id => nil,
+      :key_name => nil,
+      :security_group_ids => nil,
+    })
+      @connection.servers.select do |server|
+        next unless (options[:id] == nil) || (server.id == options[:id])
+        next unless (options[:private_ip_address] == nil) || (server.id == options[:private_ip_address])
+        next unless (options[:public_ip_address] == nil) || (server.id == options[:public_ip_address])
+        next unless (options[:dns_name] == nil) || (server.id == options[:dns_name])
+        next unless (options[:image_id] == nil) || (server.id == options[:image_id])
+        next unless (options[:key_name] == nil) || (server.id == options[:key_name])
+        next unless (options[:security_groups] == nil) || (server.id == options[:security_groups])
+        server
+      end
+    end
   end
 end
--- spec/unit/contrail/ec2_spec.rb
+++ spec/unit/contrail/ec2_spec.rb
@@ -1,12 +1,70 @@
 require 'spec_helper'
 require 'contrail/ec2'
 
+#  filter specs are all commented out until Fog mocking makes more sense
+
 describe Contrail::EC2 do
   subject(:client) {
     Fog.mock!
     Contrail::EC2.new(Contrail::Config.new('util/awsconfig').data)
   }
+  before(:each) do
+    client.connection.key_pairs.new(
+      :name => 'contrail_testkey',
+      :fingerprint => 'f00200bar'
+    ).save
+  end
+  after(:each) do
+    client.connection.key_pairs.destroy 'contrail_testkey'
+  end
   it 'initiates a new Fog EC2 connection' do
     expect(client.connection).to be_a_kind_of Fog::Compute::AWS::Mock
   end
+  it 'can list all available keys' do
+    expect(client.get_keys).to be_a_kind_of Fog::Compute::AWS::KeyPairs
+  end
+#  it 'can filter keys by name' do
+#    expect(client.get_keys(:name => 'contrail_testkey').count).to eq 1
+#    expect(client.get_keys(:name => 'contrail_fakekey').count).to eq 0
+#  end
+  it 'can list all available images' do
+    expect(client.get_images).to be_a_kind_of Fog::Compute::AWS::Images
+  end
+#  it 'can filter images by id' do
+#    expect(client.get_images(:id => 'ari-fefe73ce').count).to eq 1
+#    expect(client.get_images(:id => 'foo').count).to eq 0
+#  end
+  it 'can list all servers' do
+    expect(client.get_servers).to be_a_kind_of Fog::Compute::AWS::Servers
+  end
+#  it 'can filter servers by id' do
+#    expect(client.get_servers(:id => 'i-8c67ecbb').count).to eq 1
+#    expect(client.get_servers(:id => 'fakenum').count).to eq 0
+#  end
+#  it 'can filter servers by private ip' do
+#    expect(client.get_servers(:id => 'i-8c67ecbb').count).to eq 1
+#    expect(client.get_servers(:id => 'fakenum').count).to eq 0
+#  end
+#  it 'can filter servers by public ip' do
+#    expect(client.get_servers(:public_ip_address => '54.218.204.120').count).to eq 1
+#    expect(client.get_servers(:public_ip_address => 'fakenum').count).to eq 0
+#  end
+#  it 'can filter servers by dns name' do
+#    expect(client.get_servers(:dns_name => 'ec2-54-218-204-120.us-west-2.compute.amazonaws.com').count).to eq 1
+#    expect(client.get_servers(:dns_name => 'fakenum').count).to eq 0
+#  end
+#  it 'can filter servers by image id' do
+#    expect(client.get_servers(:image_id => 'ami-e030a5d0').count).to eq 1
+#    expect(client.get_servers(:image_id => 'fakenum').count).to eq 0
+#  end
+#  it 'can filter servers by key name' do
+#    expect(client.get_servers(:key_name => 'nil').count).to eq 1
+#    expect(client.get_servers(:key_name => 'fakenum').count).to eq 0
+#  end
+#  it 'can filter servers by a single security group' do
+#    expect(client.get_servers(:security_groups => ['sg-d21d90e2']).count).to eq 1
+#    expect(client.get_servers(:security_groups => ['fake']).count).to eq 0
+#  end
+#  it 'can filter servers by multiple security groups'
+#  it 'can filter servers by multiple indexes'
 end
