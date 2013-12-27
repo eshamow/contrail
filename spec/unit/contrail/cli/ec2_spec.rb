0b0a4f8574eebd17ae3c8154a5c5718fdbfe7ef4
Eric Shamow <eric.shamow@gmail.com>
2013-12-26 17:59:25 -0800

    Added EC2 command line unit tests

--- spec/unit/contrail/cli/ec2_spec.rb
+++ spec/unit/contrail/cli/ec2_spec.rb
@@ -4,10 +4,39 @@ require 'contrail/cli/ec2'
 
 describe Contrail::CLI::EC2 do
   describe "with ec2 command" do
-    describe "when no options are passed" do
-      it "returns help syntax" do
-        cmd = described_class.command
-        expect(cmd.name).to eq 'ec2'
+    describe "with no arguments" do
+      it "returns ec2 application" do
+        expect(described_class.command.name).to eq 'ec2'
+      end
+      it "exits with code 0" do
+        captured_output = capture_stdout do
+          expect { described_class.command.block.call(Hash.new, Array.new, described_class.command)}.to terminate.with_code 0
+        end
+      end
+      it "returns help document for ec2" do
+        captured_output = capture_stdout do
+          expect { described_class.command.block.call(Hash.new, Array.new, described_class.command)}.to terminate.with_code 0
+        end
+        expect(captured_output.gsub(/\e\[(\d+)m/, '')).to include 'contrail ec2 <subcommand> [options]'
+      end
+      describe "with -h flag" do
+        it "returns help dialog" do
+          captured_output = capture_stdout do
+            expect { described_class.command.block.call({ :help => true }, Array.new, described_class.command)}.to terminate.with_code 0
+          end
+          expect(captured_output.gsub(/\e\[(\d+)m/, '')).to include 'contrail ec2 <subcommand> [options]'
+        end
+        it "returns help dialog even if other flags are passed" do
+          captured_output = capture_stdout do
+            expect { described_class.command.block.call({ :help => true, :human => true }, Array.new, described_class.command)}.to terminate.with_code 0
+          end
+          expect(captured_output.gsub(/\e\[(\d+)m/, '')).to include 'contrail ec2 <subcommand> [options]'
+        end
+      end
+      it "returns error code if invalid flags are passed" do
+        captured_output = capture_stdout do
+          expect { described_class.command.block.call({ :foobar => true }, Array.new, described_class.command)}.to terminate.without_code 0
+        end
       end
     end
   end
